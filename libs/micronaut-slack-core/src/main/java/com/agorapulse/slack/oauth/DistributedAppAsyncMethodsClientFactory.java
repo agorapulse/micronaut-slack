576a48ddfd72be28a597048c7ceccc9a9026c4e1
musketyr <vladimir@orany.cz>
2022-09-26 21:45:48 +0200

    removed unused import

--- libs/micronaut-slack-core/src/main/java/com/agorapulse/slack/oauth/DistributedAppAsyncMethodsClientFactory.java
+++ libs/micronaut-slack-core/src/main/java/com/agorapulse/slack/oauth/DistributedAppAsyncMethodsClientFactory.java
@@ -19,7 +19,6 @@ package com.agorapulse.slack.oauth;
 
 import com.slack.api.bolt.model.Bot;
 import com.slack.api.methods.AsyncMethodsClient;
-import com.slack.api.methods.MethodsClient;
 
 import java.util.Optional;
 
